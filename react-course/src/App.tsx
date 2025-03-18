ad2cb1cb0b19d51af99d393a902b441dbe3f1324
TuanTran <tuanhoang18061997>
2025-03-18 20:58:58 +0700

    first push

---
+++ react-course/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Post from './components/Post'
+
+function App() {
+
+  return (
+    <main>
+      <Post author='Tuan Tran' body='ReactJS is awesome'/>
+      <Post author='Hoang em' body='ReactJS is awesome 123'/>
+    </main>
+  )
+}
+
+export default App
